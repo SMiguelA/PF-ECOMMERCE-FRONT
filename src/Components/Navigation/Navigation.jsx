336c8b06d8e62ad5c04921c83f639b09f93ee027
Miguel Sierra <sierrabolanosmiguel@gmail.com>
2023-06-08 15:19:37 -0500

    Nav estilos iniciales

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,3 +1,12 @@
+import { Link } from 'react-router-dom';
+import style from './Navigation.module.css';
+
 export default function Home() {
-  return <div>NAV BAR</div>;
+  return (
+    <div className={style.container}>
+      <Link to="/" className={style.noLinea}>
+        <h1>E-Commerce</h1>
+      </Link>
+    </div>
+  );
 }
