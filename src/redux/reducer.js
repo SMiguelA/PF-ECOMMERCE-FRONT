d8d3974ca59c4254f911a930f6ea7fcdb5863cab
victor <victor.lavado15@hotmail.com>
2023-06-08 16:16:45 -0500

    no quiero usar la grafica

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,15 +1,25 @@
-import { } from "./actionsTypes";
+import { GET_PRODUCTS } from "./Actions/Products/index";
+import { GET_USERS } from "./Actions/Users/index";
 
 const initialState = {
-  products:[]
+  products: [],
+  users: [],
 };
 
 const rootReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    // case : 
-    //   return {
-        
-    //   }
+    case GET_USERS:
+      return {
+        ...state,
+        users: payload,
+      };
+
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        products: payload,
+      };
+
     default:
       return { ...state };
   }
