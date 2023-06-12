b163487473f15b82a3d9059bd3bf9e94574343e2
victor <victor.lavado15@hotmail.com>
2023-06-12 09:16:07 -0500

    first commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,24 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useLocation , Outlet} from "react-router";
-import { getProducts } from "../../Redux/Actions";
-import { getUsers } from "../../Redux/Actions";
-import Slide from "./components/Slide/Slides"
+import { Outlet, useLocation } from "react-router";
 import { DivContainer } from "../../ComponentsStyles";
-import {Navbar} from "../../Components"
-import styles from "./Home.module.css"
+import { getProducts, getUsers } from "../../Redux/Actions";
+import styles from "./Home.module.css";
+import Slide from "./components/Slide/Slides";
 export default function Home() {
   const dispatch = useDispatch();
-  const location = useLocation()
+  const location = useLocation();
   useEffect(() => {
     dispatch(getProducts());
     dispatch(getUsers());
   }, []);
 
+  console.log("test");
   return (
     <>
-    {
-      (location.pathname === "/" ) &&
-      <DivContainer>
-      {/* <div className={styles.containerLink}>
+      {location.pathname === "/" && (
+        <DivContainer>
+          {/* <div className={styles.containerLink}>
               <div className={styles.firstchildLink}>
               <Navbar/>
               </div>
@@ -28,19 +26,16 @@ export default function Home() {
 
               </div>
       </div> */}
-      <div className={styles.containerHome}>
-         <div className={styles.containerSlide}>
-              <Slide/>
-         </div>
-         <div className={styles.containerCardsCategory}>
+          <div className={styles.containerHome}>
+            <div className={styles.containerSlide}>
+              <Slide />
+            </div>
+            <div className={styles.containerCardsCategory}></div>
+          </div>
+        </DivContainer>
+      )}
 
-         </div>
-      </div>
-    </DivContainer>
-    }
-
-    <Outlet/>
+      <Outlet />
     </>
-    
   );
 }
