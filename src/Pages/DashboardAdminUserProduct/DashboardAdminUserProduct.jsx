59fe569b047d159071a7eab199875ef534142a45
Miguel Sierra <sierrabolanosmiguel@gmail.com>
2023-06-26 12:32:08 -0500

    Dash Admin

--- src/Pages/DashboardAdminUserProduct/DashboardAdminUserProduct.jsx
+++ src/Pages/DashboardAdminUserProduct/DashboardAdminUserProduct.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserProducts } from '../../Redux/Actions';
+import { ProductsTable } from './components/ProductsTable';
+import { UsersTable } from './components/UsersTable';
 
 export const DashboardAdminUserProduct = () => {
-  return (
-    <div style={{padding:'5em'}}>DashboardAdminUserProduct</div>
-  )
+
+    const dispatch = useDispatch();
+    const state = useSelector(state => state.dataDashAdmin);
+    const [type, setType] = useState('users'); 
+    const [galleta, setGalleta] = useState(true);
+
+    useEffect(() => {
+        dispatch(getUserProducts());
+    },[galleta])
+
+    const handleChange = (event) => {
+        setType(event.target.value);
+    }
+    
+    return (
+        <div style={{padding:'5em'}}>
+            <select onChange={handleChange}>
+                <option value="users" selected>Users</option>
+                <option value="products">Products</option>
+            </select>
+            {
+                state 
+                ? type === 'users'
+                ? <UsersTable data={state.usersAll} galleta={galleta} setGalleta={setGalleta} />
+                : <ProductsTable data={state.productsAll} galleta={galleta} setGalleta={setGalleta} /> 
+                : <h1>Loading...</h1>
+            }
+        </div>
+    )
 }
---
+++ src/Pages/DashboardAdminUserProduct/components/UsersTable.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import DataTable from 'react-data-table-component';
+import axios from '../../../axios';
+import style from './UserTable.module.css';
+
+export const UsersTable = ({data, galleta, setGalleta}) => {
+
+    const [bandera, setBandera] = useState(null);
+    const info = data.filter((dato) =>{
+        return dato.email !== 'admin@hotmail.com'
+    })
+
+    const columns = [
+        {
+            name: 'Name',
+            selector: row => row.name,
+            sortable: true
+        },
+        {
+            name: 'Email',
+            selector: row => row.email,
+            sortable:true
+        },
+        {
+            name: 'Status', 
+            cell: (row) => (
+                <p>{row.isActive ? 'Active' : 'Banned'}</p> 
+            ),
+            sortable:true
+        },
+        {
+            name: 'N. Orders',
+            selector: row => row.__v,
+            sortable:true
+        },
+        {
+            name: 'Action', cell:(row) => (
+            bandera && bandera.email === row.email 
+            ? row.isActive 
+            ? <button id={row._id} name={row.isActive.toString()} className={style.button} onClick={handlerChangeState}>Ban</button>
+            : <button id={row._id} name={row.isActive.toString()} className={style.buttonUndo} onClick={handlerChangeState}>Unban</button>
+            :<></>
+            )
+        }
+    ]
+
+    const handlerClick = (row) => {
+        setBandera(row);
+    }
+
+    const handlerChangeState = (event) => {
+        axios.put(`/admin/${event.target.id}?type=user&isActive=${event.target.name}`)
+        .then((data) => {
+            setGalleta(!galleta);
+        }
+        ).catch((error) => alert(error))
+    }
+
+    return (
+        <DataTable 
+        columns={columns}
+        data={info}
+        highlightOnHover={true}
+        pointerOnHover={true}
+        pagination={true}
+        onRowClicked={handlerClick}
+        paginationPerPage={10}
+        />
+    )
+}
