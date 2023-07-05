27842d03bbc4bfa3b4ee0af9bec874e580c0b07c
Diegoc24 <carusodiegod@gmail.com>
2023-07-04 23:21:09 -0300

    feature

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+axios.defaults.baseURL = "http://localhost:3001"
 
-export default instance;
+export default axios;
